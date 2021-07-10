270f6ad65bea60e9dd25d87ba4901d1d4c23b9b6
Atrius <atriusedm@gmail.com>
2021-07-10 17:24:36 -0400

    Remove empty file and format server index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,44 +1,42 @@
-import { DEV } from './global';
-import { MikroORM } from '@mikro-orm/core';
-import DatabaseConfig from './config/db';
-import cors from 'cors'
-import { json } from 'body-parser'
-import session from 'express-session'
-import express from 'express'
-import setupAuth from './components/auth';
-import setupUser from './components/user';
+import { DEV } from "./global";
+import { MikroORM } from "@mikro-orm/core";
+import DatabaseConfig from "./config/db";
+import cors from "cors";
+import { json } from "body-parser";
+import session from "express-session";
+import express from "express";
+import setupAuth from "./components/auth";
+import setupUser from "./components/user";
 
 async function main() {
-    const db = await MikroORM.init(DatabaseConfig)
-    await db.getMigrator().up()
+  const db = await MikroORM.init(DatabaseConfig);
+  await db.getMigrator().up();
+  const app = express();
 
-    const app = express()
-    
-    app.use(cors({
-        // This is to ensure each endpoint on our back-end is covered by the allow access policy
-        origin: function (origin, callback) {
-            callback(null, origin)
-        },
-        // Allows the session cookie to be saved on the front end client automatically
-        credentials: true,
-    }), json())
+  app.use(
+    cors({
+      // This is to ensure each endpoint on our back-end is covered by the allow access policy
+      origin: (origin, callback) => callback(null, origin),
+      // Allows the session cookie to be saved on the front end client automatically
+      credentials: true,
+    }),
+    json(),
+    session({
+      secret: "3292583249053486903",
+      cookie: {
+        maxAge: 1000000000000,
+        httpOnly: true,
+        sameSite: "none",
+        secure: !DEV,
+      },
+      saveUninitialized: false,
+    })
+  );
 
-    // Sessions support
-    app.use(session({
-        secret: "3292583249053486903",
-        cookie: { 
-            maxAge: 1000000000000,
-            httpOnly: true,
-            sameSite: "none",
-            secure: !DEV
-         },
-        saveUninitialized: false
-    }))
+  setupAuth(app, db);
+  setupUser(app, db);
 
-    setupAuth(app, db)
-    setupUser(app, db)
-
-    app.listen(3000, () => console.log("Listening on port 3000!"))
+  app.listen(3000, () => console.log("Listening on port 3000!"));
 }
 
-main().catch((err) => console.error(err))
+main().catch((err) => console.error(err));
