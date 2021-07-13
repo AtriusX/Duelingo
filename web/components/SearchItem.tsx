667cba631aa4e8cfbaee47a4269fb23c01f318e5
Atrius <atriusedm@gmail.com>
2021-07-13 03:16:24 -0400

    Add rank to search items

--- web/components/SearchItem.tsx
+++ web/components/SearchItem.tsx
@@ -8,13 +8,13 @@ interface SearchItemData {
 }
 
 export default function SearchItem({ user }: SearchItemData) {
-    const { id, username, joined } = user
+    const { id, username, joined, rank } = user
     return (
         <Link href={`/profile/${id}`}>
             <a draggable={false} className={styles.searchitem}>
                 <Avatar className={styles.avatar} user={user} />
                 <div className={styles.info}>
-                    <h3>{username}</h3>
+                    <h3><b>({rank})</b> {username}</h3>
                     <p>Joined on {new Date(joined).toLocaleDateString()}</p>
                 </div>
             </a>
