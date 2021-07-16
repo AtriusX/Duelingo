6ad5b7277bb72f043141cbf691e023f8894b7e24
Atrius <atriusedm@gmail.com>
2021-07-16 15:00:10 -0400

    Add data faking system for development environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { DEV } from './global';
 import { MikroORM } from "@mikro-orm/core";
 import DatabaseConfig from "./config/db";
 import cors from "cors";
@@ -8,10 +9,24 @@ import setupAuth from "./components/auth";
 import setupUser from "./components/user";
 import { CorsConfig } from "./config/cors";
 import { SessionConfig } from "./config/session";
+import { User } from "./entities/User";
+import faker from "faker"
 
 async function main() {
   const db = await MikroORM.init(DatabaseConfig);
   await db.getMigrator().up();
+  // This will run a setup query to provide us with fake data in development
+  let count = await db.em.count(User, {})
+  if (DEV && count < 1000) {
+    for (let i = 0; i < 1000 - count; i++) {
+      const name = faker.name.findName()
+      const email = faker.internet.email()
+      const password = faker.internet.password()
+      db.em.persist(new User(name, email, password))
+    }
+    db.em.flush()
+  }
+
   const app = express();
   app.use(
     cors(CorsConfig), json(), session(SessionConfig)
